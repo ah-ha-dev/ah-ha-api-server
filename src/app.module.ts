7e6ac8a9f40a278ebe8ad612d9f47cac94ef7767
haron47 <pooh0407@ewhain.net>
2022-01-28 11:25:54 +0900

    init: setting dabase config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
+import databaseConfig from './common/config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => config.get('database'),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
