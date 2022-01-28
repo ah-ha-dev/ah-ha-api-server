2bae2bf07b7277f434f0071872e54ef55cda8bed
haron47 <pooh0407@ewhain.net>
2022-01-28 12:21:46 +0900

    feat: add common module to main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
+import {ValidationPipe} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import {AppModule} from './app.module';
 import {API_PREFIX, DOC_PATH} from './constants';
+import {HttpExceptionFilter} from './common/exception/httpException.filter';
+import {TransformInterceptor} from './common/interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(API_PREFIX);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   const config = new DocumentBuilder()
     .setTitle('AH-HA API docs')
