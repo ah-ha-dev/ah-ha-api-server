31103c24ab8886bc5e0595548b51facf60a3771d
haron47 <pooh0407@ewhain.net>
2022-01-29 15:41:27 +0900

    feat: getMailNumber controller

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,34 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { MailService } from './mail.service';
-import { CreateMailDto } from './dto/create-mail.dto';
-import { UpdateMailDto } from './dto/update-mail.dto';
+import {Controller, Get, Post, Body, Patch, Param, Delete, UseGuards} from '@nestjs/common';
+import {MailService} from './mail.service';
+import {JwtAuthGuard} from './../auth/guard/jwt-auth.guard';
+import {AuthUser} from './../common/decorator/user.decorator';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
 
-  @Post()
-  create(@Body() createMailDto: CreateMailDto) {
-    return this.mailService.create(createMailDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.mailService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.mailService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMailDto: UpdateMailDto) {
-    return this.mailService.update(+id, updateMailDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.mailService.remove(+id);
+  @Get('labels')
+  @UseGuards(JwtAuthGuard)
+  getMailNumber(@AuthUser() user) {
+    return this.mailService.getMailNumber(user.id);
   }
 }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,26 +1,76 @@
-import { Injectable } from '@nestjs/common';
-import { CreateMailDto } from './dto/create-mail.dto';
-import { UpdateMailDto } from './dto/update-mail.dto';
+import {Injectable} from '@nestjs/common';
+import {Mail} from './entities/mail.entity';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Repository} from 'typeorm';
+import {gmail} from '@googleapis/gmail';
+import {ConfigService} from '@nestjs/config';
+import {OAuth2Client} from 'google-auth-library';
+import {User} from './../user/entities/user.entity';
 
 @Injectable()
 export class MailService {
-  create(createMailDto: CreateMailDto) {
-    return 'This action adds a new mail';
-  }
+  constructor(
+    @InjectRepository(Mail)
+    private readonly mailRepository: Repository<Mail>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly configService: ConfigService,
+  ) {}
 
-  findAll() {
-    return `This action returns all mail`;
-  }
+  async getMailNumber(userId: number) {
+    const oAuth2Client = new OAuth2Client({
+      clientId: this.configService.get('google').googleClientId,
+      clientSecret: this.configService.get('google').googleClientSecret,
+      redirectUri: this.configService.get('google').googleRedirectUri,
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} mail`;
-  }
+    const user = await this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+      relations: ['mail'],
+    });
 
-  update(id: number, updateMailDto: UpdateMailDto) {
-    return `This action updates a #${id} mail`;
-  }
+    oAuth2Client.setCredentials({refresh_token: user.googleRefreshToken});
+    const {token} = await oAuth2Client.getAccessToken();
 
-  remove(id: number) {
-    return `This action removes a #${id} mail`;
+    oAuth2Client.setCredentials({access_token: token});
+    const googleMail = gmail({
+      version: 'v1',
+      auth: oAuth2Client,
+    });
+
+    // // 사용자 전체 리스트 가져오는 코드
+    // const labels = [];
+    // const {data} = await googleMail.users.labels.list({
+    //   userId: user.gmail,
+    // });
+    // const list = data.labels;
+
+    // for (const label of list) {
+    //   labels.push(label.name);
+    // }
+
+    // 사용자 메일함 리스트 목록
+    const labels = ['INBOX', 'SENT', 'SPAM', 'TRASH'];
+    let totalNumber = 0;
+
+    const mailList = await Promise.all(
+      labels.map(async label => {
+        const {data} = await googleMail.users.labels.get({
+          userId: user.gmail,
+          id: label,
+        });
+        totalNumber += data.threadsTotal;
+        return data;
+      }),
+    );
+
+    if (!user.mail) {
+      await this.mailRepository.save({totalNumber, user});
+    } else {
+      await this.mailRepository.update(user.mail.id, {totalNumber});
+    }
+    return {mailList};
   }
 }
