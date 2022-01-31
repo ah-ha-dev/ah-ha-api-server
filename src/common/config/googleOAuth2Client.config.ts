49a8c9e4afb5856bd355ff490fca6e6c15f2df9b
haron47 <pooh0407@ewhain.net>
2022-01-31 19:06:54 +0900

    refactor: create google auth configs

--- src/common/config/googleOAuth2Client.config.ts
+++ src/common/config/googleOAuth2Client.config.ts
@@ -1,6 +1,7 @@
 import {registerAs} from '@nestjs/config';
 import {OAuth2Client} from 'google-auth-library';
 
+// google oAuth2Client 관련 환경변수 (사용자 권한)
 export default registerAs('googleOAuth2Client', async (): Promise<OAuth2Client> => {
   return new OAuth2Client({
     clientId: process.env.GOOGLE_CLIENT_ID,
---
+++ src/common/config/googlePubSub.config.ts
@@ -0,0 +1,9 @@
+import {registerAs} from '@nestjs/config';
+
+// google PubSub 관련 환경변수
+export default registerAs('googlePubSub', () => {
+  return {
+    topicName: process.env.GOOGLE_PUBSUB_TOPIC_NAME,
+    subscriptionName: process.env.GOOGLE_PUBSUB_SUBSCRIPTION_NAME,
+  };
+});
