768d7daaf1543a23e73829135a637cbd33cdfb69
haron47 <pooh0407@ewhain.net>
2022-02-03 18:52:22 +0900

    #20 fix: add projectId to google pubsub config

--- src/common/config/googlePubSub.config.ts
+++ src/common/config/googlePubSub.config.ts
@@ -5,5 +5,6 @@ export default registerAs('googlePubSub', () => {
   return {
     topicName: process.env.GOOGLE_PUBSUB_TOPIC_NAME,
     subscriptionName: process.env.GOOGLE_PUBSUB_SUBSCRIPTION_NAME,
+    projectId: process.env.GOOGLE_PROJECT_ID,
   };
 });
