e7f0a20b739d7badd1209795d7ab6e2496c8affe
haron47 <pooh0407@ewhain.net>
2022-02-05 00:38:36 +0900

    feat: add plant kine enum vallue

--- src/plant/entities/plant.entity.ts
+++ src/plant/entities/plant.entity.ts
@@ -2,13 +2,19 @@ import {Column, Entity, OneToOne} from 'typeorm';
 import {BaseEntity} from './../../common/entity/base-entity.entity';
 import {User} from './../../user/entities/user.entity';
 
+export enum Kind {
+  GREENONION = 'GREENONION', // 대파
+  TOMATO = 'TOMATO', // 토마토
+  BROCCOLI = 'BROCCOLI', // 토마토
+}
+
 @Entity()
 export class Plant extends BaseEntity {
   @Column()
   name: string;
 
-  @Column()
-  kind: string;
+  @Column({type: 'enum', enum: Kind})
+  kind: Kind;
 
   @Column()
   score: number;
